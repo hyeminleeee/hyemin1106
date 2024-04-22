603dd5524aeebd16bde33026f3e04c99d350323d
itwill <itwill@DESKTOP-QRDHC9K>
2024-04-22 16:38:27 +0900

    hyemin1106_240422

---
+++ java_1/src/basic/FirstEclipseApp.java
@@ -0,0 +1,22 @@
+package basic;//클래스가 작성된 패키지를 표현하기 위해 사용하는 문장
+
+//주석문 : 프로그램 실행과 아무런 상관없는 설명문을 제공하기 위한 문장
+// => 유지보수의 효율성 증가를 위해 소스코드에 설명문 제공
+// => 행주석(//설명문)과 범위주석(/* 설명문 */)
+
+/*
+프로그램 이름 : FirstEclipseApp
+프로그램 버전 : 1.0
+개발도구 : JDK17 
+ */
+
+public class FirstEclipseApp {
+	public static void main(String[] args) {
+		System.out.print("이클립스로 처음 만든 프로그램입니다.");
+		System.out.println();//엔터(Enter) 출력
+		System.out.println("열심히 해봐요.");
+	}
+}
+//git repositories : github에 똑같은 repositories를 만든다.
+//Clone a Git repository : GibHub에 있는 reposiories를 똑같이 컴퓨터에 
+//local repository를 만든다.
