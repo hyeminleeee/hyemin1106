b0ea245167944c044b1a9b28c73541af1032a5f0
MsOffice96 <yangminsoo21@gmail.com>
2024-05-10 13:11:03 +0900

    240510

--- java_3/src/xyz/itwill/thread/MultiThreadApp.java
+++ java_3/src/xyz/itwill/thread/MultiThreadApp.java
@@ -6,7 +6,7 @@ package xyz.itwill.thread;
 //=> GUI 프로그램 또는 Web 프로그램 등은 다중 스레드 프로그램으로 작성
 
 //프로그램 개발자가 스레드를 생성하여 원하는 명령을 실행하는 방법 - 1
-//1. Thread 클래스를 상속받은 자식클래스 선언
+//1. Thread 클래스를 상속받은 자식클래스(내부클래스 권장) 선언
 //2. Thread 클래스를 상속받은 자식클래스에서 무조건 run() 메소드를 오버라이딩 선언
 //=> run() 메소드에는 Thread 객체에 의해 생성된 새로운 스레드가 실행하기 위한 명령 작성
 //3. Thread 클래스를 상속받은 자식클래스의 생성자로 객체 생성 - Thread 객체
@@ -20,7 +20,7 @@ package xyz.itwill.thread;
 //2. Runnable 인터페이스를 상속받은 자식클래스에서 무조건 run() 메소드를 오버라이딩 선언
 //=> run() 메소드에는 Thread 객체에 의해 생성된 새로운 스레드가 실행하기 위한 명령 작성
 //3. Runnable 인터페이스를 상속받은 자식클래스의 생성자로 객체 생성 - Runnable 객체
-//4. Thread 클래스의 매개변수가 작성된 생성자를 호출하여 Thread 객체
+//4. Thread 클래스 생성자의 매개변수가 Runnalbe 객체를 전달하여작성된 생성자를 호출하여 Thread 객체
 //=> 생성자 매개변수에 Runnable 객체를 전달하여 Thread 객체 생성
 //=> Runnable 인터페이스를 상속받은 자식클래스의 run() 메소드를 Thread 객체로 사용 가능
 //5. 생성된 Thread 객체를 참조하여 Thread 클래스의 start() 메소드 호출
@@ -83,8 +83,7 @@ public class MultiThreadApp {
 		*/
 		
 		//Runnable 인터페이스에는 반환형이 [void]이고 매개변수가 없는 run() 메소드가 하나만 
-		//작성된 함수형 인터페이스로 사용 가능
-		//=> 람다표현식을 사용하여 객체 생성 가능
+		//작성된 함수형 인터페이스로 사용 가능 - 람다표현식을 사용하여 객체 생성 가능
 		new Thread(()-> {
 				for(char i='a'; i<='z'; i++) {
 					System.out.print(i);
