c81bb6e7c4161c26aaaa9a9365ff06a05b56694d
MsOffice96 <yangminsoo21@gmail.com>
2024-07-04 19:57:05 +0900

    240704

---
+++ servlet/src/main/java/xyz/itwill/dto/StudentDTO.java
@@ -0,0 +1,68 @@
+package xyz.itwill.dto;
+
+//DTO(Data Transfer Object) 클래스 : 테이블의 행을 객체로 만들어 전달하기 위한 클래스
+
+
+//STUDENT 테이블의 행(학생정보)를 저장하여 전달하기 위한 클래스
+public class StudentDTO {
+	private int no;
+	private String name;
+	private String phone;
+	private String address;
+	private String birthday;
+	
+	public StudentDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public StudentDTO(int no, String name, String phone, String address, String birthday) {
+		super();
+		this.no = no;
+		this.name = name;
+		this.phone = phone;
+		this.address = address;
+		this.birthday = birthday;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+}
