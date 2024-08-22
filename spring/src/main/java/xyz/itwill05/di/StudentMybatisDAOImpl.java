469a46fbbb8a646f00b1052e09932da52c740836
MsOffice96 <yangminsoo21@gmail.com>
2024-08-22 20:31:52 +0900

    240822

---
+++ spring/src/main/java/xyz/itwill05/di/StudentMybatisDAOImpl.java
@@ -0,0 +1,42 @@
+package xyz.itwill05.di;
+
+import java.util.List;
+
+public class StudentMybatisDAOImpl implements StudentDAO {
+	public StudentMybatisDAOImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int insertStudent(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int updateStudent(Student student) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteStudent(int num) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Student selectStudent(int num) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Student> selectStudentList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	
+}
