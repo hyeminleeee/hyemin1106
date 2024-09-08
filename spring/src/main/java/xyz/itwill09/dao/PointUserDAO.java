baff4302a88297818591b25e2c607b8fea0e72a3
hyemin <hyemin@BOOK-I7VUQOS823>
2024-09-08 23:24:57 +0900

    240908

--- spring/src/main/java/xyz/itwill09/dao/PointUserDAO.java
+++ spring/src/main/java/xyz/itwill09/dao/PointUserDAO.java
@@ -3,7 +3,7 @@ package xyz.itwill09.dao;
 import xyz.itwill09.dto.PointUser;
 
 public interface PointUserDAO {
-	int insertPointUser(PointUser pointUser);
+	int insertPointUser(PointUser user);
 	int updatePlusPointUser(String id);
 	int updateMinusPointUser(String id);
 	PointUser selectPointUser(String id);
