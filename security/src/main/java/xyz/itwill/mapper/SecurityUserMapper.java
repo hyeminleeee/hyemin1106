e7439795be9f4932b634d63927d69aba9832fabb
MsOffice96 <yangminsoo21@gmail.com>
2024-09-20 13:21:06 +0900

    240920

---
+++ security/src/main/java/xyz/itwill/mapper/SecurityUserMapper.java
@@ -0,0 +1,10 @@
+package xyz.itwill.mapper;
+
+import xyz.itwill.dto.SecurityAuth;
+import xyz.itwill.dto.SecurityUser;
+
+public interface SecurityUserMapper {
+	int insertSecurityUser(SecurityUser user);
+	int insertSecurityAuth(SecurityAuth auth);
+
+}
