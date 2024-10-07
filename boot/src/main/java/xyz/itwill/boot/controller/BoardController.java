21c5f1e8aa75099c36386e551c9ade3e26b88007
MsOffice96 <yangminsoo21@gmail.com>
2024-10-07 15:23:43 +0900

    241007

---
+++ boot/src/main/java/xyz/itwill/boot/controller/BoardController.java
@@ -0,0 +1,40 @@
+package xyz.itwill.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.RequiredArgsConstructor;
+import xyz.itwill.boot.dto.Board;
+import xyz.itwill.boot.service.BoardService;
+
+//JPA(Java Presistence API) : ORM(Object Relational Mapping) 기능을 제공하는 표준 인터페이스
+// => ORM : 데이타베이스의 테이블과 Java 객체가 1:1로 매핑되도록 처리하는 기능
+// => SQL 명령을 작성하지 않아도 객체와 테이블이 자동으로 매핑되어 필요한 SQL 명령이 자동으로 
+//생성되어 처리 - 특정 DBMS에 종속되지 않아 유지보수의 효율성 증가
+// => 복잡한 SQL 명령을 생성하기 불편하며 테이블 결합이 많아질 경우 가독성 감소
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+	private final BoardService boardService;
+	
+	@GetMapping("/write")
+	public String write() {
+		return "board/write";
+	}
+	
+	@PostMapping("/write")
+	public String write(@ModelAttribute Board board) {
+		boardService.addBoard(board);
+		return "redirect:/board/list";
+	}
+
+	@GetMapping("/list")
+	public String list() {
+		return "board/list";
+	}
+}
---
+++ boot/src/main/java/xyz/itwill/boot/dto/Board.java
@@ -0,0 +1,26 @@
+package xyz.itwill.boot.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+import xyz.itwill.boot.entity.BoardEntity;
+
+@Data
+public class Board {
+	private Long num;//글번호
+	private String write;//작성자
+	private String title;//제목
+	private String content;//내용
+	private int hit;//조회수
+	private LocalDateTime createDate;//작성날짜
+	private LocalDateTime updateDate;//변경날짜
+	
+	public BoardEntity toBoardEntity() {
+		BoardEntity entity=new BoardEntity();
+		entity.setWriter(write);
+		entity.setTitle(title);
+		entity.setContent(content);
+		entity.setHit(0);
+		return entity;
+	}
+}
